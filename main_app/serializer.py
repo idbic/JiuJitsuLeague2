a7cb488637661146df9c2b9fa18dc5675842a561
idbic <dbicga@gmail.com>
2022-07-28 11:31:29 -0500

    redid the backend and started over

---
+++ main_app/serializer.py
@@ -0,0 +1,8 @@
+
+from rest_framework import serializers
+from . models import *
+  
+class ReactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tournament # React was put into single quotes because it was not defined causing a server disruption
+        fields = ['city', 'date', 'address', 'price', 'user']
