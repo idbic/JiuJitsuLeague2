1e1d6b7fc2cf9a70c151750aa5216ad105cda493
idbic <dbicga@gmail.com>
2022-07-30 00:14:53 -0500

    stubbed up the login form

--- backend/jiujitsuleague2/settings.py
+++ backend/jiujitsuleague2/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 REST_FRAMEWORK = {
@@ -98,7 +99,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
+    
 ]
 
 #added next two chunks for the rest action in the backend of the app
@@ -183,3 +185,5 @@ LOGIN_REDIRECT_URL = 'tournament/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
