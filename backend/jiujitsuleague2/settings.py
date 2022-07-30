71885ab6d6fad48ae3f2fc2016de2e234ca32175
idbic <dbicga@gmail.com>
2022-07-29 23:40:51 -0500

    blacklisted refresh tokens that were used

--- backend/jiujitsuleague2/settings.py
+++ backend/jiujitsuleague2/settings.py
@@ -62,7 +62,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': False,
+    'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
 
     'ALGORITHM': 'HS256',
