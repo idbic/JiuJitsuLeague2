f4e9117d3ddad78df4d43970d371e9a16732f28b
idbic <dbicga@gmail.com>
2022-07-31 14:43:38 -0500

    refactor

--- backend/main_app/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from main_app.models import Tournament
-
-class TournamentSerializer(ModelSerializer):
-    class Meta:
-        model = Tournament
-        fields = '__all__'
-
-        
--- backend/main_app/serializer.py
+++
@@ -1,8 +0,0 @@
-
-from rest_framework import serializers
-from . models import *
-  
-class ReactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tournament # React was put into single quotes because it was not defined causing a server disruption
-        fields = ['name', 'city', 'date', 'address', 'price']
